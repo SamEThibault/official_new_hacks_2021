0dc404d287a8e57417da3b22a04ca0af9a2496e0
Hunter Hoogendijk <hunter.hoogendijk@gmail.com>
2021-11-06 19:55:50 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -22,9 +22,7 @@ function App() {
     
   }, [])
   return (
-    <Container className = "d-flex align-itmes-center justify-content-center"
-      style ={{minheight: "100vh"}}>
-      <div className = "w-100" style={{maxWidth: '400px '}}>
+    <div className="App">
         <Router>
           <AuthProvider>
             <Routes>
