11913fbb41ff5202e0e43a03b8bd39efee604995
Logan Hoogendijk <logan.hoogendijk@gmail.com>
2021-11-06 19:31:54 -0400

    changed button colors

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,8 +3,34 @@ import "../styles/category.css";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom"
 import Nav from "../views/Nav"
+import { qColors } from '../styles/colors.js'
+import { makeStyles, ThemeProvider, createTheme } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  button: {
+    "&:inactive": {
+    backgroundColor: qColors.red,
+    textDecoration: "none",
+    color: 'white',
+    fontWeight: "bold",
+    },
+    "&:hover": {
+      background: qColors.red,
+      textDecoration: "none",
+      color: qColors.red,
+      fontWeight: "bold",
+    },
+    "&:active": {
+      background: qColors.red,
+      textDecoration: "none",
+      color: qColors.gold,
+      fontWeight: "bold",
+    },
+  },
+});
 
 function Category(props) {
+  const classes = useStyles()
   return (
     <div className="category-container">
       <Nav/>
@@ -13,8 +39,8 @@ function Category(props) {
         <div className="topics-container">
           {props.topics.map((topic) => (
             <div className="topic-container">
-              <Link to={topic.toLowerCase()}>
-              <Button>{topic}</Button>
+              <Link style={{textDecoration: 'none'}} to={topic.toLowerCase()}>
+              <Button style={{fontWeight: 'bold'}} className={classes.button}>{topic}</Button>
               </Link>
               </div>
           ))}
