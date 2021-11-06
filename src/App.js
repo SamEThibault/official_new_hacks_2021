e621008be2662b8b8588290f8ac04d4c6448d571
Hunter Hoogendijk <hunter.hoogendijk@gmail.com>
2021-11-06 16:18:52 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ import Dashboard from './components/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
 
 function App() {
-  useEffect(() => {
-    
-  }, [])
+
   return (
     <Container className = "d-flex align-itmes-center
     justify-content-center"
