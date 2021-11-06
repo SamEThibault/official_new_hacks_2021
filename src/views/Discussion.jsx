5874401cf1d7841fc67e79acd34f9f8daebf794b
Samuel Emard-Thibault <sam5thibault@gmail.com>
2021-11-06 16:34:16 -0400

    Update forum jsx file

--- src/views/Discussion.jsx
+++ src/views/Discussion.jsx
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from "react";
-import Button from "@mui/material/Button";
-import "../styles/discussion.css";
-import Nav from "../views/Nav"
-
-function discussion() {
-    return (
-        <div className="DiscussionMain">
-        <Nav/>
-=======
-import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import "../styles/discussion.css";
-
-function DiscussionBoard() {
-    
-    return (
-        <div className="DiscussionMain">
->>>>>>> 4381b7b (commiting to rebase forum branch)
-            <div className="NewPostBtn">
-                <h1>Discussion Board</h1>
-                <Button
-                    variant="contained"
-<<<<<<< HEAD
-                    style={{
-=======
-                    style=
-                    {{
->>>>>>> 4381b7b (commiting to rebase forum branch)
-                        background: "#1f3a60",
-                        height: '90%',
-                        width: '90%'
-                    }}
-                >Post Message</Button>
-            </div>
-
-            <div className="TextBox">
-<<<<<<< HEAD
-                <textarea 
-=======
-                <textarea
->>>>>>> 4381b7b (commiting to rebase forum branch)
-                    style={{
-                        position: "fixed",
-                        top: 230,
-                        width: 600,
-                        height: 300,
-                        left: 10
-                    }}
-                >Type your message here... </textarea>
-            </div>
-        </div>
-    );
-}
-
-<<<<<<< HEAD
-export default discussion;
-=======
-export default DiscussionBoard;
->>>>>>> 4381b7b (commiting to rebase forum branch)
-=======
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -115,4 +52,3 @@ function Discussion() {
 }
 
 export default Discussion;
->>>>>>> 0e71cb0 (formatting, started the output UI for discussion forum)
