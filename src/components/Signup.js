7e73172073deb6d00c8fd6e9ea3a480d483d304c
Logan Hoogendijk <logan.hoogendijk@gmail.com>
2021-11-06 17:17:06 -0400

    fixed layout of signup and login pages

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -33,16 +33,15 @@ async function handleSubmit(e){
     setLoading(false)
 }
 
-
     return (
         <>
-            <Card>
-                <Card.Body>
+            <Card className ="m-auto align-self-center" style={{maxWidth: 500}}>
+                <Card.Body className ="m-auto align-self-center" style={{width: 370}}>
                     <h2 className="text-center mb-4"> Sign Up</h2>
                    {currentUser && currentUser.password}
                     {error && <Alert variant="danger"> {error}</Alert>}
                     <Form onSubmit = {handleSubmit}>
-                        <Form.Group id="email">
+                        <Form.Group  id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type = "email" ref={emailRef} required />
                         </Form.Group>
@@ -62,6 +61,7 @@ async function handleSubmit(e){
                 Already have an account? <Link to="/login">Log In</Link>
 
             </div>
+
         </>
     )
 }
