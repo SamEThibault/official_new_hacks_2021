b5223cbe63815dcf3bb68225fe0ab26cd41b1f52
Kevin <yu.kevin2002@gmail.com>
2021-11-06 17:22:58 -0400

    few changes

--- src/App.js
+++ src/App.js
@@ -14,31 +14,33 @@ import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
+import topicNote from "./components/TopicNote"
 
 function App() {
 
   return (
-    <Container className = "d-flex align-itmes-center
-    justify-content-center"
-      style ={{minheight: "100vh"}}>
-      <div className = "w-100" style={{maxWidth: '400px '}}>
-        <Router>
-          <AuthProvider>
-            <Routes>
-              <Route exact path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
-              <Route exact path="*" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
-              <Route exact path ="signup" element={<Signup />}/>
-              <Route exact path ="login" element={<Login />}/>
-              <Route path="home" element={<Home />} />
-              <Route path="study" element={<NoteUpload />} />
-              <Route path="discussion" element={<Discussion />} />
-              {/* <Route path="*" element={<PageNotFound/>} /> */}
-            </Routes>
-          </AuthProvider>
-        </Router>
-      </div>
+    // <Container className = "d-flex align-itmes-center
+    // justify-content-center"
+    //   style ={{minheight: "100vh"}}>
+    //   <div className = "w-100" style={{maxWidth: '400px '}}>
+    //     <Router>
+    //       <AuthProvider>
+    //         <Routes>
+    //           <Route exact path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+    //           <Route exact path="*" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+    //           <Route exact path ="signup" element={<Signup />}/>
+    //           <Route exact path ="login" element={<Login />}/>
+    //           <Route path="home" element={<Home />} />
+    //           <Route path="study" element={<NoteUpload />} />
+    //           <Route path="discussion" element={<Discussion />} />
+    //           {/* <Route path="*" element={<PageNotFound/>} /> */}
+    //         </Routes>
+    //       </AuthProvider>
+    //     </Router>
+    //   </div>
 
-    </Container>
+    // </Container>
+    <TopicNote/>
   );
 }
 
