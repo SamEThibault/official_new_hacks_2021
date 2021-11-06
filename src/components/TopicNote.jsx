4b06e7ad9923346a46087f59d98ebeb05714aa86
Kevin <yu.kevin2002@gmail.com>
2021-11-06 16:34:30 -0400

    fixed changes

--- src/components/TopicNote.jsx
+++ src/components/TopicNote.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import "./NoteUpload";
-=======
 import tempPdf from "../assets/Note_Nov_6_2021.pdf";
 import NoteUpload from "./NoteUpload";
->>>>>>> kevin
 import "../styles/topicNote.css";
 function TopicNote() {
   const noteData = [
@@ -17,25 +13,15 @@ function TopicNote() {
       <div className="container">
         {noteData.map((note, key) => (
           <div className="note-taker-container">
-<<<<<<< HEAD
-            <h1 className="note-taker-name">{note.name}</h1>
-            <div className="note-taker-info">
-              <img className="note-taker-notes" src="" alt={note.img} />
-              <p>{note.message}</p>
-=======
             <h1 className="note-taker-name">{`${note.name}'s Notes`}</h1>
             <div className="note-taker-info">
               <p>{note.message}</p>
               <embed className="note-pdf" src={tempPdf} type="application/pdf"></embed>
->>>>>>> kevin
             </div>
             <p>Download</p>
           </div>
         ))}
-<<<<<<< HEAD
-=======
         <NoteUpload />
->>>>>>> kevin
       </div>
     </div>
   );
