0163750abf0799f0307a57140fd5e4a4e24dc449
Kevin <yu.kevin2002@gmail.com>
2021-11-07 09:39:39 -0500

    added full functionality

--- src/components/TopicNote.jsx
+++ src/components/TopicNote.jsx
@@ -1,50 +1,69 @@
-import React, { useState } from "react";
-import tempPdf from "../assets/Note_Nov_6_2021.pdf";
+import React, { useState, useEffect } from "react";
 import NoteUpload from "./NoteUpload";
 import "../styles/topicNote.css";
-import Nav from "../views/Nav"
-function TopicNote() {
-  const [newNote, setNewNote] = useState(false);
-  const noteData = [
-    { name: "Kevin", img: "Photo here pelase", message: "HI" },
-    { name: "Logan", img: "PLEAS BRO", message: "ITS ME" },
-    { name: "Hunter", img: "HIASFJLJK", message: "Please like Comment" },
-  ];
+import Nav from "../views/Nav";
+import firebase from "../firebase/firebase";
+import {useDispatch, useSelector} from "react-redux";
+import {selectNewNote, setNewNote} from "../features/noteDataSlice";
+import { BsTrash} from "react-icons/bs";
+import {CgNotes} from "react-icons/cg"
+function TopicNote(props) {
+  const dispatch = useDispatch();
+  const newNote = useSelector(selectNewNote);
+
+  const notedb = firebase.firestore().collection("Notes");
+
+  const deleteNote = async (id, file) => {
+    try {
+      await notedb.doc(id).delete();
+      if (file !== null) {
+        await firebase.storage().refFromURL(file).delete();
+      }
+    } catch {
+      console.log("Error with deleting post");
+    }
+  };
   return (
     <div className="topic-note-container">
-      <Nav/>
+      <Nav />
       <div className="container">
-        {noteData.map((note, key) => (
+        {props.notes.map((note, key) => (
           <div className="note-taker-container">
             <h1 className="note-taker-name">{`${note.name}'s Notes`}</h1>
             <div className="note-taker-info">
-              <p>{note.message}</p>
-              <embed className="note-pdf" src={tempPdf} type="application/pdf"></embed>
+              <embed className="note-pdf" src={note.file} type="application/pdf"></embed>
             </div>
-            <p>Download</p>
+            <button
+            className="delete-note"
+              onClick={() => {
+                deleteNote(note.id, note.file);
+              }}
+            >
+              <BsTrash/>
+            </button>
           </div>
         ))}
 
         {newNote ? (
           <>
-            <NoteUpload />
+            <NoteUpload topic={props.topic} />
             <div
               className="new-note-icon"
               onClick={() => {
-                setNewNote(!newNote);
+                dispatch(setNewNote(!newNote));
               }}
             >
-              New Note
+              <CgNotes/>
             </div>
           </>
         ) : (
           <div
             className="new-note-icon"
             onClick={() => {
-              setNewNote(!newNote);
+              dispatch(setNewNote(!newNote));
             }}
           >
-            New Note
+            <CgNotes/>
           </div>
         )}
       </div>
